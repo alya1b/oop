da21134952ef3a9b2616f56c5b3187f56b0bb9ec
alya1b <93484747+alya1b@users.noreply.github.com>
2021-11-18 02:06:44 +0200

    Add files via upload

---
+++ AbstractTriangle/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace AbstractTriangle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Прямокутний трикутник";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(610, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Рівнобедрений трикутник";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(213, 80);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(213, 122);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(661, 74);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 10;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(661, 122);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(111, 80);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Катет a";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(111, 122);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 17);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Катет b";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(544, 74);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(64, 17);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Сторона";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(559, 117);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(31, 17);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Кут";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(181, 191);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 44);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Площа";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(631, 181);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(119, 44);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "Площа";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(631, 309);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(119, 47);
+            this.button5.TabIndex = 21;
+            this.button5.Text = "Периметр";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(181, 319);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(119, 47);
+            this.button6.TabIndex = 22;
+            this.button6.Text = "Периметр";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(191, 266);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 23;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(640, 255);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 22);
+            this.textBox11.TabIndex = 26;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(640, 390);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(100, 22);
+            this.textBox12.TabIndex = 27;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(191, 390);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(100, 22);
+            this.textBox13.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 553);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+    }
+}
+
---
+++ AbstractTriangle/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AbstractTriangle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        abstract class Triangle
+        {
+            public double a;
+            public double b;
+            public double ang_c; //angle in radians
+
+            public virtual void CountArea(ref double area)
+            {
+                area = a * b * Math.Sin(ang_c);
+            }
+            public virtual void CountPerimeter(ref double perimeter)
+            {
+                perimeter = Math.Sqrt(b * b + a * a - 2 * b * a * Math.Cos(ang_c)) + a + b;
+            }
+            public void Change_a(double newa)
+            {
+                a = newa;
+            }
+            public void Change_b(double newb)
+            {
+                b = newb;
+            }
+            public void Change_angle(double newangle)
+            {
+                ang_c = newangle;
+            }
+        }
+        class Right_Triangle : Triangle
+        {
+            public override void CountArea(ref double area)
+            {
+                area = a * b /2;
+            }
+            public override void CountPerimeter(ref double perimeter)
+            {
+                perimeter = Math.Sqrt(b * b + a * a) + a + b;
+            }
+
+        }
+        class Isosceles_Triangle : Triangle
+        {
+            public override void CountArea(ref double area)
+            {
+                area = a * a * Math.Sin(ang_c);
+            }
+            public override void CountPerimeter(ref double perimeter)
+            {
+                perimeter = Math.Sqrt(2 * a * a - 2 * a * a * Math.Cos(ang_c)) + a * 2;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double result = 0;
+            string a = textBox4.Text;
+            string b = textBox5.Text;
+            Right_Triangle A = new Right_Triangle();
+
+            A.Change_a(Convert.ToDouble(a));
+            A.Change_b(Convert.ToDouble(b));
+
+            A.CountArea(ref result);
+
+            textBox8.Text = result.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            double result = 0;
+            string a = textBox4.Text;
+            string b = textBox5.Text;
+            Right_Triangle A = new Right_Triangle();
+
+            A.Change_a(Convert.ToDouble(a));
+            A.Change_b(Convert.ToDouble(b));
+
+            A.CountPerimeter(ref result);
+
+            textBox13.Text = result.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double result = 0;
+            string a = textBox6.Text;
+            string angle = textBox7.Text;
+            Isosceles_Triangle A = new Isosceles_Triangle();
+
+            A.Change_a(Convert.ToDouble(a));
+            A.Change_angle(Convert.ToDouble(angle));
+
+            A.CountArea(ref result);
+
+            textBox11.Text = result.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double result = 0;
+            string a = textBox6.Text;
+            string angle = textBox7.Text;
+            Isosceles_Triangle A = new Isosceles_Triangle();
+
+            A.Change_a(Convert.ToDouble(a));
+            A.Change_angle(Convert.ToDouble(angle));
+
+            A.CountPerimeter(ref result);
+
+            textBox12.Text = result.ToString();
+        }
+    }
+}
